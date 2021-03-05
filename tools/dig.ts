da8093842fc42a6a8ef3d8d5d080ed3744dd1af8
Thejesh GN <i@thejeshgn.com>
2021-03-05 19:53:00 +0530

    Added example dig.ts

---
+++ tools/dig.ts
@@ -0,0 +1,11 @@
+// From the denoland 1.7 examples
+
+const domainName = Deno.args[0];
+if (!domainName) {
+  throw new Error("Domain name not specified in first argument");
+}
+
+const records = await Deno.resolveDns(domainName, "A");
+for (const ip of records) {
+  console.log(ip);
+}
